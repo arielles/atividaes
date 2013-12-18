33df9fb0f75ff8cb4ea9db681dabdda16880026e
arielles <ariellesmarques@gmail.com>
2013-12-18 16:24:52 -0200

    Create index.php

---
+++ texto/index.php
@@ -0,0 +1,18 @@
+<?php
+$x = 9;
+$y = 12;
+
+$dia = 11;
+$mes = 12;
+$ano = 2013;
+$texto = "A data de hoje e ";
+
+echo $texto . $dia . "/" . $mes . "/" . $ano;
+echo "<br/>";
+echo "O dia de hoje e $dia/$mes/$ano";
+echo "<br/>";
+echo "$x + $y = " . ($x + $y);
+
+
+
+?>
