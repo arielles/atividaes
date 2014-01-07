4522b45b699acc8760a6c92dde4eb1ac7b408339
arielles <ariellesmarques@gmail.com>
2014-01-07 18:31:30 -0200

    Create menu.php

---
+++ cadastro/menu.php
@@ -0,0 +1,8 @@
+<center>
+<a href="index.php">Listar</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover Cadastros</a>
+<br/><br/>
+</center>
