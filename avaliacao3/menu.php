e5523f01cbdf462da1187947fec9d86ccebff1c9
arielles <ariellesmarques@gmail.com>
2014-01-13 18:31:08 -0200

    Create menu.php

---
+++ avaliacao3/menu.php
@@ -0,0 +1,12 @@
+<center>
+<fieldset>
+<a href="index.php">Lista de produtos</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar produtos</a>
+&nbsp;&nbsp;
+<a href="form_editar.php">Editar produtos</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover produtos</a>
+<br/><br/>
+</fieldset>
+</center>
